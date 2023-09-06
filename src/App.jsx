ec0b089fa953b3c48b4d67e6b41e4f5b4082fbf6
hy219 <jhy_0219@daum.net>
2023-09-06 23:31:39 +0900

    init recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { ActivityIcon, CheckCircleIcon } from '@goorm-dev/gds-goormthon';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <ActivityIcon width="1rem" className="ActivityIcon__icon" />
-      Hello earth
-    </div>
+    <RecoilRoot>
+      <div className="text-3xl font-bold underline">
+        <ActivityIcon width="1rem" className="ActivityIcon__icon" />
+        Hello earth
+      </div>
+    </RecoilRoot>
   );
 }
 
