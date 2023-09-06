ba6d41258513b653d130a42435ad2229df5c07a5
hy219 <jhy_0219@daum.net>
2023-09-06 17:36:39 +0900

    init install gds

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ActivityIcon, CheckCircleIcon } from '@goorm-dev/gds-goormthon';
 
 function App() {
-  return <div className="text-3xl font-bold underline">Hello earth</div>;
+  return (
+    <div className="text-3xl font-bold underline">
+      <ActivityIcon width="1rem" className="ActivityIcon__icon" />
+      Hello earth
+    </div>
+  );
 }
 
 export default App;
