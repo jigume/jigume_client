be37dde7d86b4840a477d5543ef3cd1dee5840b0
도경 <lee7198@naver.com>
2023-09-07 15:48:10 +0900

    feat #1 add icons

--- src/pages/Map/components/BottomSheetComponent/components/ItemList.jsx
+++ src/pages/Map/components/BottomSheetComponent/components/ItemList.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Filter from '../../../../../asset/icon/Filter.svg';
 import ItemComponent from './ItemComponent';
+import categories from '../data';
 
 export default function ItemList() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <div className="relative top-0 border-b border-gray100">
@@ -13,10 +16,37 @@ export default function ItemList() {
           <div className="caption text-gray600">
             제품군 카테고리 필터를 켜보세요
           </div>
-          <img src={Filter} className="w-[24px] h-[24px]" />
+
+          <div
+            className="h-3 cursor-pointer hover:text-black"
+            onClick={() => setOpen((prev) => !prev)}
+          >
+            {open ? '완료' : <img src={Filter} className="w-[24px] h-[24px]" />}
+          </div>
         </div>
       </div>
-      <ItemComponent />
+      <div className="flex flex-wrap">
+        {categories.map((item) => (
+          <div
+            key={item.idx}
+            className="p-[8px] border border-gray-100 rounded-lg"
+          >
+            <img
+              className="inline-block mr-2 w-[16px] h-[16px]"
+              src={item.icon}
+            />
+            <span className="caption">{item.name}</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <ItemComponent />
+        <ItemComponent />
+        <ItemComponent />
+        <ItemComponent />
+        <ItemComponent />
+        <ItemComponent />
+      </div>
     </div>
   );
 }
