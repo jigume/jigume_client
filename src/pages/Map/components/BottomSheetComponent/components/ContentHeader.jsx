d9837dbaba9a21583cbe8eb0a12dfd2c209b5e72
도경 <lee7198@naver.com>
2023-09-07 22:35:57 +0900

    feat #1 category filter

--- src/pages/Map/components/BottomSheetComponent/components/ContentHeader.jsx
+++ src/pages/Map/components/BottomSheetComponent/components/ContentHeader.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CloseIcon } from '@goorm-dev/gds-goormthon';
 import CategoryTag from './CategoryTag';
 import categories from '../data';
 import Filter from '../../../../../asset/icon/Filter.svg';
@@ -17,13 +18,40 @@ export default function ContentHeader() {
         <div className="px-[16px] py-[12px] ">
           <div className="h4 mb-0">공동 구매 폼 내역 보기</div>
         </div>
-        <div className="px-[16px] py-[12px] flex flex-row justify-between">
-          <div className="caption text-gray600">
-            제품군 카테고리 필터를 켜보세요
+        <div className="px-[16px] py-[12px] grid grid-cols-8">
+          <div className="col-span-7 h-[1.5rem] overflow-x-scroll   whitespace-nowrap text-gray600">
+            {filter.find(({ checked }) => checked) ? (
+              filter.map(
+                (item, index) =>
+                  item.checked && (
+                    <span
+                      className="px-1 cursor-pointer"
+                      key={item.name}
+                      onClick={() => {
+                        const prevData = [...filter];
+
+                        prevData[index] = {
+                          ...prevData[index],
+                          checked: !item.checked,
+                        };
+
+                        setFilter(prevData);
+                      }}
+                    >
+                      <span>{item.name}</span>
+                      <CloseIcon className="inline-block pb-[3px] " />
+                    </span>
+                  ),
+              )
+            ) : (
+              <div className="caption text-gray600">
+                제품군 카테고리 필터를 켜보세요
+              </div>
+            )}
           </div>
 
           <div
-            className="h-3 cursor-pointer hover:text-black"
+            className="col-span-1 h-[1.5rem] flex justify-center cursor-pointer hover:text-black "
             onClick={() => setOpen((prev) => !prev)}
           >
             {open ? '완료' : <img src={Filter} className="w-[24px] h-[24px]" />}
