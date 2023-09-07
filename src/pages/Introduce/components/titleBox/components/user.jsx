8d595e131e78fed120a049ac00b00e51f746825f
hy219 <jhy_0219@daum.net>
2023-09-08 06:55:56 +0900

    introuce page style  modify2

--- src/pages/Introduce/components/titleBox/components/user.jsx
+++ src/pages/Introduce/components/titleBox/components/user.jsx
@@ -6,9 +6,9 @@ export default function User() {
   return (
     <div className="flex flex-row pb-[20px] mb-[12px] place-content-center">
       <UserImage />
-      <div className="flex flex-row w-[295px]">
+      <div className="flex flex-row w-[295px] text-[14px]">
         <UserName />
-        <div className="flex flex-col justify-center text-center text-gray600 pl-[3px]">
+        <div className="flex flex-col justify-center text-center text-gray-600 pl-[3px] text-[14px]">
           <div>| 6번째 구매 리드</div>
         </div>
       </div>
