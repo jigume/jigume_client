0a8433d271c6d97f0fa2d1fec6af02314dca41c0
hy219 <jhy_0219@daum.net>
2023-10-19 18:43:03 +0900

    픽업 위치 지정 화면 출력 & 공지 페이지로 이동

--- src/pages/Register/components/getPlace/component/postcode.jsx
+++ src/pages/Register/components/getPlace/component/postcode.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { React, useState } from 'react';
 import DaumPostcodeEmbed from 'react-daum-postcode';
 
 function Postcode() {
+  const [open, setOpen] = useState(false);
+  const [address, setAddress] = useState('');
+
+  const handlePlaceChange = () => {
+    setOpen(false); // 날짜 선택 후 모달 닫기
+  };
+
   const handleComplete = (data) => {
     let fullAddress = data.address;
     let extraAddress = '';
@@ -18,9 +25,33 @@ function Postcode() {
     }
 
     console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+    setAddress(fullAddress);
   };
 
-  return <DaumPostcodeEmbed onComplete={handleComplete} />;
+  return (
+    <div>
+      <button
+        data-modal-target="defaultModal"
+        data-modal-toggle="defaultModal"
+        className="border rounded-md w-full h-78 p-3 text-sm w-full  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-left"
+        type="button"
+        onClick={() => setOpen((prev) => !prev)}
+        placeholder="서울 성동구 왕십리로2길 20"
+      >
+        {address || (
+          <div className="text-gray-400">서울 성동구 왕십리로2길 20</div>
+        )}
+      </button>
+      {open && (
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
+          <DaumPostcodeEmbed
+            onComplete={handleComplete}
+            onChange={handlePlaceChange}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Postcode;
--- src/pages/Register/components/getPlace/index.jsx
+++ src/pages/Register/components/getPlace/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Postcode from './component/postcode';
 
 function GetPlace() {
   return (
-    <form className="flex flex-col">
-      <div className="mt-1 h-10 w-20 bg-slate-200">픽업 주소검색</div>
-      <div>주소 가져오는 api 사용하기</div>
-      <Postcode />
-      <input
-        type="submit"
-        value="공구 등록하기"
-        className="mt-1 h-10 w-50 bg-slate-200"
-      />
-    </form>
+    <div className="w-full h-[calc(100svh-48px)] flex flex-col mt-52">
+      <div />
+      <div className="text-lg font-bold pb-10">
+        어디서 공구 물품을 전달할까요?
+        <br />
+        팔로워들과 만날 픽업 장소를 알려주세요.
+      </div>
+      <div className="">
+        <div className="align-top text-sm mb-2 font-thin">픽업 위치 지정</div>
+        <Postcode className="border rounded-md w-full p-3 text-sm font-medium mb-10 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5" />
+      </div>
+      <Link
+        to="/Register/ProductNotice"
+        className="w-full py-3 my-3 text-center bg-success text-white rounded-lg"
+      >
+        다음으로 넘어가기
+      </Link>
+    </div>
   );
 }
 
