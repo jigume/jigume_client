b6ccd813bcceaef3636550e777ef158bb4853f4e
hy219 <jhy_0219@daum.net>
2023-11-06 17:39:11 +0900

    refact #3 text컴포넌트, svg이미지 분리

--- src/pages/Announcement/components/CommentsContent.jsx
+++ src/pages/Announcement/components/CommentsContent.jsx
@@ -1,39 +1,25 @@
 import React from 'react';
 import User from './User';
+import CommentsText from './CommentsText';
+import addCommentIcon from '../../../asset/icon/addCommentIcon.svg';
 
 export default function CommentsContent() {
   return (
     <div className="container mx-auto max-w-screen-sm mt-8 bg-white">
       <div className="border-b-2 border-gray-100 px-5 py-5 font-medium text-[14px]">
-        댓글
+        댓글 23
       </div>
       <User />
       <div className="text-[14px] px-5">
-        <div className="flex flex-row m-4 pl-7">
-          현대수산 앞 맞죠?? 저는 10일이든 13일이든 6시 이후기만 하면 시간 다
-          괜찮습니다.
-        </div>
+        <CommentsText />
         <div className="flex flex-row m-4 pl-7">
           <div className="align-middle p-1">
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M1.5 1.5V5.26125L9 6L1.5 6.73875V10.5L11.25 6L1.5 1.5Z"
-                fill="#858899"
-              />
-            </svg>
+            <img src={addCommentIcon} />
           </div>
           <div className="align-middle text-gray-600">답글달기</div>
         </div>
         <div className="m-4 pl-7 align-middle text-gray-600 text-cyan-400 font-medium">
-          이전 답글 n개 더보기
+          이전 답글 1개 더보기
         </div>
       </div>
       <div className="mt-24"> </div>
---
+++ src/pages/Announcement/components/CommentsText.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CommentsText() {
+  return (
+    <div className="flex flex-row m-4 pl-7 ">
+      현대수산 앞 맞죠?? 저는 10일이든 13일이든 6시 이후기만 하면 시간 다
+      괜찮습니다.
+    </div>
+  );
+}
