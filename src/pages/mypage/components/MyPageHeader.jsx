0c6b93b9f61285a769fefcb9bf550bed65889213
도경 <lee7198@naver.com>
2023-12-21 13:37:14 +0900

    faet #11 마이페이지 퍼블리싱

---
+++ src/pages/mypage/components/MyPageHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import ChevronLeft from '../../../asset/icon/chevron-left.svg';
+
+export default function MyPageHeader() {
+  const navigate = useNavigate();
+  return (
+    <div className="mx-auto flex h-[48px] w-full max-w-screen-sm flex-row items-center px-4">
+      <div onClick={() => navigate(-1)} className="pr-2">
+        <img className="h-12 w-12 cursor-pointer p-2" src={ChevronLeft} />
+      </div>
+      <div className="font-bole mt-1">마이페이지</div>
+    </div>
+  );
+}
--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import { useResetRecoilState } from 'recoil';
-import { useMutation } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { authState } from '../../recoil';
 import { kakaoLogout } from '../../api/user';
+import MyPageHeader from './components/MyPageHeader';
+import getProfile from '../../api/mypage';
 
 export default function Mypage() {
   const resetAuth = useResetRecoilState(authState);
@@ -15,16 +18,46 @@ export default function Mypage() {
     },
   });
 
+  // useQuery를 사용하여 fetch 함수 실행 (getProfile)
+  const { data: profile } = useQuery('getProfile', () => getProfile(), {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
   return (
-    <div>
-      Mypage
-      <button
-        className='
+    <>
+      <MyPageHeader />
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-4 p-4">
+          <img className="h-14 w-14 rounded-xl bg-zinc-300" />
+          <div>
+            <div className="text-base">졸린 춘식이 &gt;</div>
+            <div className="text-sm text-zinc-500">아직은 공동구매 새싹이</div>
+          </div>
+        </div>
+
+        <div className="p-4">
+          <div className="text-zinc-700">활동</div>
+        </div>
+
+        <div className="p-4">
+          <div className="text-lg text-zinc-900">판매내역</div>
+        </div>
+        <div className="h-[1px] w-full bg-zinc-200" />
+        <div className="p-4">
+          <div className="text-lg text-zinc-900">구매내역</div>
+        </div>
+        <div className="h-[1px] w-full bg-zinc-200" />
+
+        <button
+          className='
         className="h-12 text-md active:disabled:scale-100" w-full rounded-lg p-3 text-center text-red-600 transition-all duration-300 ease-in-out active:scale-[99%] disabled:bg-gray-300'
-        onClick={() => resetAuth()}
-      >
-        로그아웃
-      </button>
-    </div>
+          onClick={() => resetAuth()}
+        >
+          로그아웃
+        </button>
+      </div>
+    </>
   );
 }
