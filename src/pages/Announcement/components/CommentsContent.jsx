7c752d6deca65d1cc56654a5fa0e8249a51d6af9
도경 <lee7198@naver.com>
2024-01-18 16:53:51 +0900

    feat #3 공지방 UI 구현

--- src/pages/Announcement/components/CommentsContent.jsx
+++ src/pages/Announcement/components/CommentsContent.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import User from './User';
 
 export default function CommentsContent() {
   return (
-    <div className="container mx-auto max-w-screen-sm px-0">
-      <div className="text-[14px]">
-        현대수산 앞 맞죠?? 저는 10일이든 13일이든 6시 이후기만 하면 시간 다
-        괜찮습니다.
+    <div className="container mx-auto max-w-screen-sm mt-8 bg-white">
+      <div className="border-b-2 border-gray-100 px-5 py-5 font-medium text-[14px]">
+        댓글
       </div>
+      <User />
+      <div className="text-[14px] px-5">
+        <div className="flex flex-row m-4 pl-7">
+          현대수산 앞 맞죠?? 저는 10일이든 13일이든 6시 이후기만 하면 시간 다
+          괜찮습니다.
+        </div>
+        <div className="flex flex-row m-4 pl-7">
+          <div className="align-middle p-1">
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M1.5 1.5V5.26125L9 6L1.5 6.73875V10.5L11.25 6L1.5 1.5Z"
+                fill="#858899"
+              />
+            </svg>
+          </div>
+          <div className="align-middle text-gray-600">답글달기</div>
+        </div>
+        <div className="m-4 pl-7 align-middle text-gray-600 text-cyan-400 font-medium">
+          이전 답글 n개 더보기
+        </div>
+      </div>
+      <div className="mt-24"> </div>
     </div>
   );
 }
---
+++ src/pages/Announcement/components/User.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Avatar from 'boring-avatars';
+import {
+  primaryBlue,
+  primaryJade,
+  primaryPurple,
+  primaryYello,
+} from '../../../common';
+
+const nickname = [
+  'jeju',
+  'hackathon',
+  'goorm',
+  'kakao',
+  '카카dh',
+  '구름좋아',
+  '클라우드',
+  '눈사람',
+  '무지',
+  '라이언',
+  '춘식이',
+  '춘삼이',
+  '카카오맵',
+];
+
+export default function User({ title, username, count }) {
+  const num = Math.floor(Math.random() * 10);
+  return (
+    <div className="flex flex-row items-center gap-2 pt-7 px-5">
+      {!username ? (
+        <div className="w-[36px] h-[36px] rounded-full bg-gray-100" />
+      ) : (
+        <Avatar
+          size={18}
+          name={title}
+          variant="beam"
+          colors={[
+            primaryYello,
+            primaryJade,
+            primaryPurple,
+            '#C271B4',
+            primaryBlue,
+          ]}
+        />
+      )}
+      {/* <div className="w-[18px] h-[18px] rounded-full bg-gray-100" /> */}
+      <div className="flex flex-row caption gap-1 items-center">
+        {!username ? (
+          <div className="w-32 h-[36px] bg-gray-100 rounded-sm" />
+        ) : (
+          <div>{nickname[num]}</div>
+        )}
+      </div>
+    </div>
+  );
+}
