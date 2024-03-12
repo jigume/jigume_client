d2d5a963c97d4792ea3a58385db980a934392ca9
도경 <lee7198@naver.com>
2024-03-12 20:35:33 +0900

    update

--- src/common.ts
+++ src/common.ts
@@ -5,7 +5,7 @@ export const primaryJade = '#8CDDE2';
 export const primaryPurple = '#E4CCFF';
 export const primaryBlue = '#0D99FF';
 
-export const backURL = 'http://43.201.105.35:8080';
+export const backURL = 'https://43.201.105.35:8080';
 // export const backURL = 'http://172.30.1.62:8080';
 
 export const prohibition = {
