edcf7529d253033b415a5672432ba9faad96f02c
도경 <lee7198@naver.com>
2024-04-13 22:52:40 +0900

    ㅕㅔㅇㅁㅅㄷ

--- src/pages/Map/components/BottomSheetComponent/index.tsx
+++ src/pages/Map/components/BottomSheetComponent/index.tsx
@@ -32,7 +32,6 @@ export default function BottomSheetComponent({
     mutationKey: 'getSheetDetail',
     mutationFn: () => getSheetGoods(preViewer, auth.accessToken as string),
     onSuccess: (res) => {
-      console.log(res);
       if (res === 'retry') preViewMutate(preViewer);
       else
         setGoodsArr([
